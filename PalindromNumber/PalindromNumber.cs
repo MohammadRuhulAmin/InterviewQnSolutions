18033e31889e392177eae1f8ede6cb1a4eb12b18
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-08-24 07:43:35 +0600

    Check a Number is Pallindrom or Not

---
+++ PalindromNumber/PalindromNumber.cs
@@ -0,0 +1,20 @@
+namespace enosis_solutions.PalindromNumber
+{
+    public class PalindromNumber
+    {
+        public  void checkPalindromOrNot(int number){
+            int newNumb = 0;
+            int storeNumb = number;
+            while(number>0){
+                int temp = number%10;
+                number/=10;
+                newNumb*=10;
+                newNumb+=temp;
+            }
+            Console.WriteLine("The new numb is :"+newNumb);
+            if(newNumb == storeNumb)Console.WriteLine("Palindrom Number");
+            else Console.WriteLine("Not A Palindrom Number");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using enosis_solutions.ReverseString;
+ using enosis_solutions.ReverseString;
+ using enosis_solutions.PalindromNumber;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -7,9 +8,28 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+
+            /*
+                Given a Sentence you have to reverse all the cherecters.. !
+                Input : This is a good Guy
+                Output : sihT si a doog yuG
+            */
+            
             ReverseString revString = new ReverseString();
             string text = "Hi This is Md Ruhul Amin";
             Console.WriteLine(revString.reverseString(text));
+            
+
+            /*
+                Given a Number you have to tell is it pallindrom or not 
+                Input : 12321 
+                Output : Palindrom 
+            */
+            
+            PalindromNumber pnum = new PalindromNumber();
+            int number_1 = 12321;
+            pnum.checkPalindromOrNot(number_1);
+
         }
     }
 }
